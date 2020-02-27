830e1e3871de1ed33a54accede3626ed9f708490
kentaylor92 <ken_seaway@gmail.com>
2020-02-27 12:39:08 -0500

    Updates

---
+++ precache-manifest.09547872257574fd762205579a94d7a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2515c473c17b7dcec179ea207c0916ff",
+    "url": "/kenneth-taylor-project-five/index.html"
+  },
+  {
+    "revision": "b35481901389bed7395a",
+    "url": "/kenneth-taylor-project-five/static/css/main.dca88bc0.chunk.css"
+  },
+  {
+    "revision": "23aa491e2bb852e75181",
+    "url": "/kenneth-taylor-project-five/static/js/2.07b7e3e1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kenneth-taylor-project-five/static/js/2.07b7e3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35481901389bed7395a",
+    "url": "/kenneth-taylor-project-five/static/js/main.ad8916d3.chunk.js"
+  },
+  {
+    "revision": "9ab93bc68fea8125937d",
+    "url": "/kenneth-taylor-project-five/static/js/runtime-main.cc43d20a.js"
+  },
+  {
+    "revision": "b3ef288994167891181a57d7ff42b226",
+    "url": "/kenneth-taylor-project-five/static/media/bookshelf.b3ef2889.jpg"
+  }
+]);
