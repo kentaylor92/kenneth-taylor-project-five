8d1c52ec6e29a54a88e895492803faa9e93eb9a3
kentaylor92 <ken_seaway@gmail.com>
2020-02-27 16:36:07 -0500

    linked userInput to Axios call, updated styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './index.css';
 import DisplayBooks from './components/DisplayBooks';
 
 class App extends Component {
@@ -9,10 +9,44 @@ class App extends Component {
 
     this.state = {
       books: [],
+      userInput: '',
     }
   }
 
   componentDidMount() {
+    // const apiKey = 'AIzaSyBN0p9j4hgZ700Jnyt2zz9QwMx9BIdcjW4';
+
+    // axios({
+    //   url: 'https://www.googleapis.com/books/v1/volumes',
+    //   method: 'GET',
+    //   responseType: 'json',
+    //   params: {
+    //     key: apiKey,
+    //     q: this.state.userInput,
+    //     startIndex: 0,
+    //     maxResults: 16,
+    //   }
+    // }).then((response) => {
+    //   console.log(response.data.items);
+
+    //   this.setState({
+    //     books: response.data.items,
+    //   })
+    // })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      userInput: e.target.value, 
+      
+    })
+    // console.log(e.target.value);
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('The following text was submitted!', + this.state.value);
+
     const apiKey = 'AIzaSyBN0p9j4hgZ700Jnyt2zz9QwMx9BIdcjW4';
 
     axios({
@@ -21,21 +55,22 @@ class App extends Component {
       responseType: 'json',
       params: {
         key: apiKey,
-        q: 'react',
+        q: this.state.userInput,
         startIndex: 0,
         maxResults: 16,
       }
     }).then((response) => {
-      console.log(response);
+      console.log(response.data.items);
 
       this.setState({
         books: response.data.items,
       })
     })
+
+    this.setState({
+      userInput: '',
+    })
   }
-
-
-
 
   render() {
     return (
@@ -43,6 +78,18 @@ class App extends Component {
         <header className="App-header">
             <h1>Book Search App</h1>
           <div className="wrapper">
+            <form action="submit" onSubmit={this.handleFormSubmit}>
+              <label htmlFor='bookSearch'>Search by book name, author, or subject.</label>
+              <input
+                type='text'
+                id='bookSearch'
+                onChange={this.handleChange}
+                value={this.state.userInput}
+                placeholder='Search by book name, author, or subject.'
+              />
+              <button type='submit'>Search for books</button>
+              
+            </form>
           </div>
         </header>
         <section className="results wrapper">
